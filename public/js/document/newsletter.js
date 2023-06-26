b01f6894c60f1690a36228454c501c3545ef53f0
Christian F <kontakt+github@christian-feldkirchner.at>
2023-06-26 08:46:09 +0200

    [Improvement] Added class to `tabBar` (#27)
    * added class to `tabBar`
    
    * Update composer.json

--- public/js/document/newsletter.js
+++ public/js/document/newsletter.js
@@ -116,6 +116,9 @@ pimcore.document.newsletter = Class.create(pimcore.document.page_snippet, {
         }
 
         this.tabbar = new Ext.TabPanel({
+            tabBar: {
+                cls: 'pimcore_editor_tabbar'
+            },
             tabPosition: "top",
             region:'center',
             deferredRender:true,
